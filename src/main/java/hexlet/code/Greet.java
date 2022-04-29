34c48c0c2fb709b23ede69cf2534b736f1935a2e
Svirshko L <esvirshko@assist.ru>
2022-04-29 16:30:19 +0300

    Перенос Greet в отдельный класс

---
+++ src/main/java/hexlet/code/Greet.java
@@ -0,0 +1,22 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Greet {
+    private static String name = "";
+
+    public static String getName() {
+        return name;
+    }
+
+    public static String getGreeting() {
+        Scanner scannerName = new Scanner(System.in);
+
+        System.out.println("Welcome to the Brain Games! \n"
+                + "May I have your name?");
+
+        name = scannerName.nextLine();
+
+        return "Hello, " + name + "!";
+    }
+}
